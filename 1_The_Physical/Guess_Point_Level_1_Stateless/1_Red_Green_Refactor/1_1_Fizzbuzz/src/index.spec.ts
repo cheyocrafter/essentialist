e3c46fdf993a36976ffc25a191aefd69e0dfd551
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-03-19 11:13:20 -0500

    [NEW] - returns 'FizzBuzz' if the number is a multiple of 3 and 5

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -12,4 +12,8 @@ describe("fizzbuzz", () => {
   it("should return 'Buzz' if the number is a multiple of 5", () => {
     expect(fizzBuzz(5)).toBe("Buzz");
   })
+
+  it("should return 'FizzBuzz' if the number is a multiple of 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+  })
 });
