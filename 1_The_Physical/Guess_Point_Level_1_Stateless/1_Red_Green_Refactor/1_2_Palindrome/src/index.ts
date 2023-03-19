cd53b083a43437d3ea1f6af6efc12b1556834fed
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-03-19 13:08:26 -0500

    [REFACTOR] - add return type to isPalindrome function definition

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,5 +1,5 @@
 export class PalindromeChecker {
-  isPalindrome(word: string) {
+  isPalindrome(word: string): boolean {
     const reversedWord = word.split('').reverse().join('');
     return reversedWord.toLowerCase().split(' ').join('') === word.toLowerCase().split(' ').join('');
   }
