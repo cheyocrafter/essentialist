fbc2afd57d06220d27c29c4dd46e1a1699e27a3f
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-03-19 13:06:15 -0500

    [REFACTOR] - add palindromeChecker object to beforeEach jest hook to eliminate duplication on every test

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,37 +1,37 @@
 import { PalindromeChecker } from './index';
 
 describe('palindrome checker', () => {
+  let palindromeChecker: PalindromeChecker;
+  
+  beforeEach(() => {
+    palindromeChecker = new PalindromeChecker();
+  });
+
   it("should show that palindrome class is defined", () => {
     expect(PalindromeChecker).toBeDefined();
   }); 
 
   it("should detect that 'mom' is a palindrome", () => {
-    const palindromeChecker = new PalindromeChecker();
     expect(palindromeChecker.isPalindrome("mom")).toBeTruthy();
   });
 
   it("should detect that 'wow' is a palindrome", () => {
-    const palindromeChecker = new PalindromeChecker();
     expect(palindromeChecker.isPalindrome("wow")).toBeTruthy();
   });
   
   it("should detect that 'bill' is a palindrome", () => {
-    const palindromeChecker = new PalindromeChecker();
     expect(palindromeChecker.isPalindrome("bill")).toBeFalsy();
   });
   
   it("should detect that 'bill' is a palindrome", () => {
-    const palindromeChecker = new PalindromeChecker();
     expect(palindromeChecker.isPalindrome("Mom")).toBeTruthy();
   });
 
   it("should detect that 'Was It A Rat I Saw' is a palindrome", () => {
-    const palindromeChecker = new PalindromeChecker();
     expect(palindromeChecker.isPalindrome("Was It A Rat I Saw")).toBeTruthy();
   });
 
   it("should detect that 'Never Odd or Even' is a palindrome", () => {
-    const palindromeChecker = new PalindromeChecker();
     expect(palindromeChecker.isPalindrome("Never Odd or Even")).toBeTruthy();
   });
 });
