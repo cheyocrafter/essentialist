a81ad4734a58d223282280e1d83af832867a9a89
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-03-19 12:16:42 -0500

    [New] - shows that the palindrome checker class is defined

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,7 @@
+import { PalindromeChecker } from './index';
 
 describe('palindrome checker', () => {
-
-})
+  it("should show that palindrome class is defined", () => {
+    expect(PalindromeChecker).toBeDefined();
+  }); 
+});
