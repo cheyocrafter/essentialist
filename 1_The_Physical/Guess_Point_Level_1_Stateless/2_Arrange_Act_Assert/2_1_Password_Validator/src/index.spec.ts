7ff7d90bd7b9374dfa64f568201175af8f03241f
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-03-20 20:40:31 -0500

    [NEW] - it should return the correct error object if the password is between 5 and 15 characters

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -4,5 +4,49 @@ describe('PasswordValidator', () => {
   it("should be defined", () => {
     expect(PasswordValidator).toBeDefined()
   });
+
+  describe("Password Length Validation", () => {
+    it("should return isValid:true and errors:[] if password length is between 5 and 15 charaters", () => {
+      const passwordValidationResult = {
+        isValid: false,
+        errors: []
+      };
+
+      const passwordValidator = new PasswordValidator(); 
+      expect(passwordValidator.validate("password")).toEqual(passwordValidationResult)
+    });
+
+    it("should return the correct error object if the password length is less than 5 characters", () => {
+      const passwordValidationResult = {
+        isValid: false,
+        errors: [
+          { 
+            type: "PASSWORD_LENGTH_ERROR", 
+            message: "password must be between 5 and 15 characters" 
+          }
+        ]
+      };
+
+      const passwordValidator = new PasswordValidator(); 
+      expect(passwordValidator.validate("pass")).toEqual(passwordValidationResult);
+    })
+
+    it("should return the correct error object if the password length is greater than 15 characters", () => {
+      const passwordValidationResult = {
+        isValid: false,
+        errors: [
+          { 
+            type: "PASSWORD_LENGTH_ERROR", 
+            message: "password must be between 5 and 15 characters" 
+          }
+        ]
+      };
+
+      const passwordValidator = new PasswordValidator(); 
+      expect(passwordValidator.validate("pass")).toEqual(passwordValidationResult);
+    })
+    
+  })
+
 });
 
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -1,3 +1,18 @@
 export class PasswordValidator {
-  
+  validate(password: string): { isValid: boolean, errors: {type: string, message: string}[]} {
+    const validationResult = {
+      isValid: false,
+      errors: [] as {type: string, message: string}[]
+    };
+
+    if (password.length < 5 || password.length > 15) {
+      const error = {
+        type: "PASSWORD_LENGTH_ERROR", 
+        message: "password must be between 5 and 15 characters"
+      }
+      validationResult.errors.push(error)
+    }
+
+    return validationResult
+  }
 }
