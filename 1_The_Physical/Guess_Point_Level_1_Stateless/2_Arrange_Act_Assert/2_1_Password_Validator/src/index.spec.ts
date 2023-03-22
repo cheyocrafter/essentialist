99481c8d2d9cc7dab4743f2af6c7829aa10556ab
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-03-22 14:44:29 -0500

    [NEW] - detects a password with an invalid minimum length

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -18,5 +18,21 @@ describe('password validator', () => {
     expect(Array.isArray(validationResult.errors)).toBe(true);
     expect(validationResult.errors).toEqual([]);
   })
+
+  it("should return the correct error object when given a password with an invalid minimum length", () => {
+      // arrange 
+      const password = "pass";
+      const passwordValidator = new PasswordValidator();
+
+      // act 
+      const validationResult = passwordValidator.validate(password)
+
+      // assert
+      expect(validationResult.isValid).toBeFalsy();
+      expect(Array.isArray(validationResult.errors)).toBe(true);
+      expect(validationResult.errors.length).toBe(1);
+      expect(validationResult.errors[0].type).toMatch(/MIN_LENGTH_ERROR/);
+      expect(validationResult.errors[0].message).toMatch(/A password must have a minimum of 5 characters/);
+  })
 });
 
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -1,8 +1,21 @@
 export class PasswordValidator {
   validate(password: string) {
-    return {
+    const validationResult = {
       isValid: true, 
-      errors: []
+      errors: [] as {type: string, message: string}[]
     }
+
+    if (password.length < 5) {
+      validationResult.errors.push({
+        type: 'MIN_LENGTH_ERROR',
+        message: "A password must have a minimum of 5 characters"
+      })
+    }
+
+    if (validationResult.errors.length !== 0) {
+      validationResult.isValid = false;
+    }
+
+    return validationResult;
   }
 }
