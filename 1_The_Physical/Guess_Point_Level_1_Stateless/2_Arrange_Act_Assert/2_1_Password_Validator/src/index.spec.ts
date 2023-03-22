5917611c040407cb5db69d2ccc336d0c904ad4ed
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-03-22 17:48:42 -0500

    set errors manager before each test and pass it through dependency injection

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -1,7 +1,16 @@
 import { PasswordValidator } from './index';
-import { Password } from "./password"
+import { Password } from "./password";
+import { errorsConfigList } from './errors-config';
+import { ErrorsManager, IErrorsManager } from './errors-manager';
+
 
 describe('password validator', () => {
+  let errorsManager: IErrorsManager;
+
+  beforeEach(() => {
+    errorsManager = new ErrorsManager(errorsConfigList);
+  })
+
   it("should detect that the 'PasswordValidtor' is defined", () => {
     expect(PasswordValidator).toBeDefined();
   });
@@ -9,7 +18,7 @@ describe('password validator', () => {
   it("should return the correct error object when a password is valid", () => {
     // Arrange
     const password = new Password("Password123"); 
-    const passwordValidator = new PasswordValidator(password); 
+    const passwordValidator = new PasswordValidator(password, errorsManager); 
     
     // Act 
     const validationResult = passwordValidator.validate()
@@ -23,7 +32,7 @@ describe('password validator', () => {
   it("should return the correct error object when given a password with an invalid minimum length", () => {
       // arrange 
       const password = new Password("1Pas");
-      const passwordValidator = new PasswordValidator(password);
+      const passwordValidator = new PasswordValidator(password, errorsManager);
 
       // act 
       const validationResult = passwordValidator.validate();
@@ -32,14 +41,14 @@ describe('password validator', () => {
       expect(validationResult.isValid).toBeFalsy();
       expect(Array.isArray(validationResult.errors)).toBe(true);
       expect(validationResult.errors.length).toBe(1);
-      expect(validationResult.errors[0].type).toMatch(/MIN_LENGTH_ERROR/);
-      expect(validationResult.errors[0].message).toMatch(/A password must have a minimum length of 5 characters/);
+      expect(validationResult.errors[0].type).toMatch(/VALID_LENGTH_ERROR/);
+      expect(validationResult.errors[0].message).toMatch(/A password must be between 5 and 15 characters/);
   });
 
   it("should return the correct error object when given a password with an invalid maximum length", () => {
     // arrange 
     const password = new Password("Thisisaverylongpasswordwith39characters");
-    const passwordValidator = new PasswordValidator(password);
+    const passwordValidator = new PasswordValidator(password, errorsManager);
 
     // act 
     const validationResult = passwordValidator.validate();
@@ -48,14 +57,14 @@ describe('password validator', () => {
     expect(validationResult.isValid).toBeFalsy();
     expect(Array.isArray(validationResult.errors)).toBe(true);
     expect(validationResult.errors.length).toBe(1);
-    expect(validationResult.errors[0].type).toMatch(/MAX_LENGTH_ERROR/);
-    expect(validationResult.errors[0].message).toMatch(/A password must have a maximum length of 15 characters/);
+    expect(validationResult.errors[0].type).toMatch(/VALID_LENGTH_ERROR/);
+    expect(validationResult.errors[0].message).toMatch(/A password must be between 5 and 15 characters/);
   });
 
   it("should return the correct error object when given a password without at least 1 digit", () => {
     // arrange 
     const password = new Password("Password");
-    const passwordValidator = new PasswordValidator(password);
+    const passwordValidator = new PasswordValidator(password, errorsManager);
 
     // act 
     const validationResult = passwordValidator.validate();
@@ -71,7 +80,7 @@ describe('password validator', () => {
   it("should return the correct error object when given a password without at least 1 uppercase letter", () => {
     // arrange 
     const password = new Password("passw0rd");
-    const passwordValidator = new PasswordValidator(password);
+    const passwordValidator = new PasswordValidator(password, errorsManager);
 
     // act 
     const validationResult = passwordValidator.validate();
@@ -86,7 +95,7 @@ describe('password validator', () => {
 
   it("should return the correct object when given a password with a valid length, but without digits or uppercase letters", () => {
     const password = new Password("password");
-    const passwordValidator = new PasswordValidator(password);
+    const passwordValidator = new PasswordValidator(password, errorsManager);
 
     // act 
     const validationResult = passwordValidator.validate();
@@ -104,7 +113,7 @@ describe('password validator', () => {
   it("should return the correct error object when given a password without correct length, no digit, or uppercase letters", () => {
     // arrange 
     const password = new Password("ok");
-    const passwordValidator = new PasswordValidator(password);
+    const passwordValidator = new PasswordValidator(password, errorsManager);
 
     // act 
     const validationResult = passwordValidator.validate();
@@ -113,8 +122,8 @@ describe('password validator', () => {
     expect(validationResult.isValid).toBeFalsy(); 
     expect(Array.isArray(validationResult.errors)).toBe(true);
     expect(validationResult.errors.length).toBe(3);
-    expect(validationResult.errors[0].type).toMatch(/MIN_LENGTH_ERROR/);
-    expect(validationResult.errors[0].message).toMatch(/A password must have a minimum length of 5 characters/);
+    expect(validationResult.errors[0].type).toMatch(/INVALID_LENGTH_ERROR/);
+    expect(validationResult.errors[0].message).toMatch(/A password must be between 5 and 15 characters/);
     expect(validationResult.errors[1].type).toMatch(/NO_DIGIT_ERROR/);
     expect(validationResult.errors[1].message).toMatch(/A password must contain at least 1 digit/);
     expect(validationResult.errors[2].type).toMatch(/NO_UPPERCASE_LETTER_ERROR/);
