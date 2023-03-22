643adebd972659d01b3eb8aa29745144e38b081b
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-03-22 16:46:03 -0500

    [REFACTOR] - add ErrorsManager class for error handling in PasswordValidator

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/error.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/error.ts
@@ -3,3 +3,39 @@ export interface IErrorItem {
   message: string;
 }
 
+export interface IErrorsManager {
+  errors: IErrorItem[];
+  configErrorList: IErrorItem[];
+  add: (error: string) => void;
+  hasErrors: () => boolean;
+  getErrors: () => IErrorItem[];
+}
+
+export class ErrorsManager implements IErrorsManager {
+  errors: IErrorItem[];
+  configErrorList: IErrorItem[]
+
+  constructor(configList: IErrorItem[]) {
+    this.configErrorList = configList;
+    this.errors = [];
+  }
+
+  add = (errorType: string): void => {
+    const foundError = this.configErrorList.find((error: IErrorItem) => {
+      return error.type === errorType;
+    })
+
+    if (!foundError) {
+      throw new Error("The error you're trying to add does not exist")
+    }
+    this.errors.push(foundError)
+  }
+
+  hasErrors = (): boolean => {
+    return this.errors.length > 0;
+  }
+
+  getErrors = (): IErrorItem[] => {
+    return this.errors;
+  }
+}
