ef0b9aa844e922c6a58092f728ad36ac642b51bc
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-03-23 10:56:13 -0500

    [NEW] - returns the average of a sequence of numbers using 12 decimal places of precision

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -31,4 +31,28 @@ describe('stats calculator', () => {
     expect(result).toBe(53)
   });
 
+  it("should return '6' after counting the numbers in the sequence '[1, 2, 3, 4, 5, 6]'", () => {
+    // arrange 
+    const sequence = [1, 2, 3, 4, 5, 6]; 
+    const statsCalculator = new StatsCalculator(); 
+
+    // act 
+    const result = statsCalculator.count(sequence); 
+
+    // assert
+    expect(result).toBe(6)
+  });
+
+  it("should return the average '18.666666666667' rounded to 12 decimal places given the sequence '[2, 4, 21, -8, 53, 40]'", () => {
+    // arrange 
+    const sequence = [2, 4, 21, -8, 53, 40];
+    const statsCalculator = new StatsCalculator(); 
+
+    // act 
+    const result = statsCalculator.average(sequence);
+
+    // assert 
+    expect(result).toBe(18.666666666667);
+  });
+
 })
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
@@ -9,4 +9,14 @@ export class StatsCalculator {
     const sorted = sequence.sort((a, b) => a - b);
     return sorted[sorted.length-1];
   }
+
+  count(sequence: Array<number>): number {
+    return sequence.length;
+  }
+
+  average(sequence: Array<number>): number {
+    const sum = sequence.reduce((acc, curr) => acc + curr, 0); 
+    const num = sequence.length; 
+    return parseFloat((sum / num).toFixed(12));
+  }
 }
