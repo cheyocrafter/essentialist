eccbb38367309b9a94977a2b06e2c4a018c90ff5
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-03-23 11:37:01 -0500

    [REFACTOR] - refactor test for finding minimum value to use it.each() to pass multiple values to test

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -7,17 +7,23 @@ describe('stats calculator', () => {
     expect(StatsCalculator).toBeDefined()
   })
 
-  it("should find that '-8' is the smallest integers from the sequence '[2, 4, 21, -8, 53, 40]'", () => {
-    // arrange 
-    const sequence = [2, 4, 21, -8, 53, 40];
-    const statsCalculator = new StatsCalculator();
-
-    // act 
-    const result = statsCalculator.minimum(sequence);
-    
-    // assert 
-    expect(result).toBe(-8);
-  });
+  describe('Return Minimum', () => {
+    it.each([
+      {expected: -8, sequence: [2, 4, 21, -8, 53, 40]},
+      {expected: -298, sequence: [-298, -100, 3, 8574, 2]},
+      {expected: 47, sequence: [5899, 84, 20000, 47, 400]},
+      {expected: 11, sequence: [83, 274, 11, 57, 23]},
+    ])(`should return '$expected' since it is the smallest integer in the sequence '$sequence'`, ({ expected, sequence }) => {
+      // const sequence = [2, 4, 21, -8, 53, 40];
+      const statsCalculator = new StatsCalculator();
+  
+      // act 
+      const result = statsCalculator.minimum(sequence);
+      
+      // assert 
+      expect(result).toBe(expected);
+    });
+  })
 
   it("should find that '53' is the largest value in the '[2, 4, 21, -8, 53, 40]' sequence", () => {
     // arrange 
