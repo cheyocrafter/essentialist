4080b9b5f52c948c03a9718cc3175684de4362d4
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-03-23 11:50:51 -0500

    [REFACTOR] - replace single test for .count() with multiple ones using it.each() jest method

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -41,19 +41,27 @@ describe('stats calculator', () => {
       // assert 
       expect(result).toBe(expected);
     });
-  })
+  }); 
 
-  it("should return '6' after counting the numbers in the sequence '[1, 2, 3, 4, 5, 6]'", () => {
-    // arrange 
-    const sequence = [1, 2, 3, 4, 5, 6]; 
-    const statsCalculator = new StatsCalculator(); 
+  describe('statsCalculator.count()', () => {
+    it.each([
+      {expected: 8, sequence: [-298, -100, 3, 8574, 2, 47, 29, 8]},
+      {expected: 7, sequence: [2, 4, 21, -8, 53, 40, 9]},
+      {expected: 3, sequence: [83, 274, 11]},
+      {expected: 10, sequence: [5899, 84, 20000, 47, 400, 100, 200, 300, 38, 0]},
+    ])
+    ("should return '6' after counting the numbers in the sequence '[1, 2, 3, 4, 5, 6]'", ({ expected, sequence }) => {
+      // arrange 
+      const statsCalculator = new StatsCalculator(); 
+  
+      // act 
+      const result = statsCalculator.count(sequence); 
+  
+      // assert
+      expect(result).toBe(expected)
+    });
+  }); 
 
-    // act 
-    const result = statsCalculator.count(sequence); 
-
-    // assert
-    expect(result).toBe(6)
-  });
 
   it("should return the average '18.666666666667' rounded to 12 decimal places given the sequence '[2, 4, 21, -8, 53, 40]'", () => {
     // arrange 
