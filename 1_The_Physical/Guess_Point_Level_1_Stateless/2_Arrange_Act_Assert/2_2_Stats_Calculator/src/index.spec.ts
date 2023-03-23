a33d1a94e265e82135e1530bc347bf6d27ea4af2
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-03-23 10:10:14 -0500

    [NEW] - detects that StatsCalculator class is defined

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,5 +1,11 @@
+import { StatsCalculator } from "./index"
+
 
 describe('stats calculator', () => {
+  it("should detect that 'StatsCalculator' is defined", () => {
+    // arrange, act, assert
+    expect(StatsCalculator).toBeDefined()
+  })
 
 
 })
