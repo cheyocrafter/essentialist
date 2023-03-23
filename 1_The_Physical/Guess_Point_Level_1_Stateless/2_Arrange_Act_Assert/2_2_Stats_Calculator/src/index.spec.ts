98f4822c71ee23cdf4ec16789f3dc02d3a68d5ef
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-03-23 10:37:12 -0500

    [NEW] - returns the largest number in a sequence of integers

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -19,4 +19,16 @@ describe('stats calculator', () => {
     expect(result).toBe(-8);
   });
 
+  it("should find that '53' is the largest value in the '[2, 4, 21, -8, 53, 40]' sequence", () => {
+    // arrange 
+    const sequence = [2, 4, 21, -8, 53, 40];
+    const statsCalculator = new StatsCalculator()
+
+    // act 
+    const result = statsCalculator.maximum(sequence);
+
+    // assert
+    expect(result).toBe(53)
+  });
+
 })
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
@@ -1,7 +1,12 @@
 export class StatsCalculator {
-  minimum(sequence: Array<number>) {
+  minimum(sequence: Array<number>): number {
     return sequence.reduce((acc, curr) => {
       return acc = (acc > curr) ? curr : acc, acc
     }, Infinity);
+  }
+
+  maximum(sequence: Array<number>): number {
+    const sorted = sequence.sort((a, b) => a - b);
+    return sorted[sorted.length-1];
   }
 }
