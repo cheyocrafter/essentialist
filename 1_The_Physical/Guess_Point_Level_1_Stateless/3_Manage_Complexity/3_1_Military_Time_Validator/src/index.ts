5e232037de4c2397dd0e3e6bd171515143179f5f
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-03-24 09:44:26 -0500

    [NEW] - process array of times into a nested array of parsed hours and minutes (both integers)

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -3,8 +3,17 @@ export class MilitaryTimeValidator {
     return timeRange.split(" - ");
   }
   
+  processTime(timeRangeList: Array<string>) {
+    return timeRangeList.map((timeRange: string) => {
+      const [hours, minutes] = timeRange.split(":");
+      return [parseInt(hours, 10), parseInt(minutes, 10)];
+    });
+  }
+  
   validate(timeRange: string): boolean {
     const processedTimeRange = this.processTimeRange(timeRange);
+    const processedTime = this.processTime(processedTimeRange);
+    console.log(processedTime);
     return true;
   }
 }
