fb8bf7144812839021ce399725e960ce30d72953
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-03-24 09:11:38 -0500

    [NEW] - it is able to tell whether the 'MilitaryTimeValidator' class is defined

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,3 +1,9 @@
-describe('military time validator', () => {
+import { MilitaryTimeValidator } from "./index";
 
+describe('military time validator', () => {
+  it("should be able to tell that the 'MilitaryTimeValidator' class is defined", () => {
+    expect(MilitaryTimeValidator).toBeDefined();
+  });
+
+  
 });
