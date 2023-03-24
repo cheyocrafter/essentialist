7cb2098f2460d8be5c0d4097fac1dda9b2c3280a
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-03-24 11:07:14 -0500

    [REFACTOR] - replace valid and invalid single tests with validation array of objects and it.each() jest method to run multiple tests at once

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -5,11 +5,28 @@ describe('military time validator', () => {
     expect(MilitaryTimeValidator).toBeDefined();
   });
 
-  it("should be able to tell a string time range is valid", () => {
+  it.each([
+    { timeRange: "01:38 - 22:33",  expected: true, message: "valid" },
+    { timeRange: "03:15 - 18:45",  expected: true, message: "valid" },
+    { timeRange: "07:30 - 16:15",  expected: true, message: "valid" },
+    { timeRange: "12:00 - 23:59",  expected: true, message: "valid" },
+    { timeRange: "06:45 - 14:30",  expected: true, message: "valid" },
+    { timeRange: "00:00 - 07:00",  expected: true, message: "valid" },
+    { timeRange: "14:00 - 22:00",  expected: true, message: "valid" },
+    { timeRange: "08:30 - 17:45",  expected: true, message: "valid" },
+    { timeRange: "02:00 - 12:00",  expected: true, message: "valid" },
+    { timeRange: "23:00 - 01:00",  expected: true, message: "valid" },
+    { timeRange: "19:00 - 23:00",  expected: true, message: "valid" },
+    { timeRange: '25:15 - 18:45',  expected: false, message: "invalid" }, 
+    { timeRange: '12:00 - 60:59',  expected: false, message: "invalid" }, 
+    { timeRange: '24:00 - 00:00',  expected: false, message: "invalid" }, 
+    { timeRange: '12:60 - 13:45',  expected: false, message: "invalid" }, 
+    { timeRange: '09:00 - 100:00', expected: false, message: "invalid" }, 
+    { timeRange: '400:00 - 500:00', expected: false, message: "invalid" }, 
+  ])
+  ('should return that the time range "$timeRange" is "$message"', ({ timeRange, expected }) => {
     // arrange
-    const timeRange = "01:12 - 14:32";
     const militaryTimeValidator = new MilitaryTimeValidator();
-    const expected = true; 
 
     // act 
     const actual = militaryTimeValidator.isValidRange(timeRange);
@@ -17,17 +34,6 @@ describe('military time validator', () => {
     // assert 
     expect(actual).toBe(expected);
   });
-
-  it("should be albe to tell a string time range is invalid", () => {
-    // arrange 
-    const timeRange = "25:00 - 12:23"; 
-    const militaryTimeValidator = new MilitaryTimeValidator();
-    const expected = false; 
-
-    // act 
-    const actual = militaryTimeValidator.isValidRange(timeRange);
-
-    // assert 
-    expect(actual).toBe(expected);
-  })
 });
+
+
