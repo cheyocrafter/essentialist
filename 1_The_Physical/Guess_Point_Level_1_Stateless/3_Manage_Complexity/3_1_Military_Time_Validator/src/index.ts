a86034963af8d5b53f8bae51d7aa8f1c0491151e
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-03-24 10:11:04 -0500

    [NEW] - add isValidTime to validate a time string in the format '01:12' or '25:38' represents a military time or not

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -8,10 +8,15 @@ export class MilitaryTimeValidator {
     return [parseInt(hours, 10), parseInt(minutes, 10)];
   }
 
-  
+  private isValidTime(time: string): boolean {
+    const [hours, minutes] = this.processTime(time);
+    return (hours >= 0 && hours <= 23) && (minutes >= 0 && minutes <= 59)
+  }
   
   validate(timeRange: string): boolean {
     const [startTime, endTime] = this.processTimeRange(timeRange);
-    return this.isValidTime(startTime);
+    console.log(this.isValidTime(startTime));
+    console.log(this.isValidTime(endTime));
+    return this.isValidTime(startTime) && this.isValidTime(endTime);
   }
 }
