ad6063389c04c9b33e2fb32e36fde70629cf4c13
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-04-02 11:54:03 -0500

    test: returns true if MilitaryTimeValidator class is defined

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,5 +1,9 @@
+import { MilitaryTimeValidator } from './index';
+
 
 describe('military time validator', () => {
+  it("should be defined", () => {
+    expect(MilitaryTimeValidator).toBeDefined();
+  });
 
-
-})
+});
