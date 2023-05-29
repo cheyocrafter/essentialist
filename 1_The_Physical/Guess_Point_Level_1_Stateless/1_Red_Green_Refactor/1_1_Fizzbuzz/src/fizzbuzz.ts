9a39c1a2f81157f8fd0582366dbd8b15008c0066
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-05-29 14:04:58 -0500

    [refactor]: add multiple of 5 boolean flag

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,11 +1,12 @@
 export function fizzBuzz(num: number) {
   const isMultipleOfThree = num % 3 === 0; 
-  const isMultipleOfThreeAndFive = num % 3 === 0 && num % 5 == 0;
+  const isMultipleOfFive = num % 5 === 0; 
+  const isMultipleOfThreeAndFive = isMultipleOfThree && isMultipleOfFive;
 
 
   if (isMultipleOfThreeAndFive) return 'FizzBuzz';
   if (isMultipleOfThree) return 'Fizz';
-  if (num % 5 === 0) return 'Buzz'
+  if (isMultipleOfFive) return 'Buzz'
   return num.toString();
 }
 
