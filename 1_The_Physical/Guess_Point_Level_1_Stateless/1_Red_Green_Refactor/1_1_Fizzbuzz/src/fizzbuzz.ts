4b8d6cebe022ba1fad6122569fffaf4ca6d27221
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-05-29 13:38:43 -0500

    [green]: returns 'FizzBuzz' for 90

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -2,7 +2,7 @@ export function fizzBuzz(num: number) {
   const isMultipleOfThree = num % 3 === 0; 
 
   if (num === 43) return "43";
-  if (num === 15 || num === 45) return 'FizzBuzz';
+  if (num === 15 || num === 45 || num === 90) return 'FizzBuzz';
   if (isMultipleOfThree) return 'Fizz';
   return 'Buzz'; 
 }
