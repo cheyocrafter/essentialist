b28e545d3765fb0fff69c17cd7e20d744e6ae2da
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-05-29 14:31:15 -0500

    [green]: it throws an error for numbers > 100

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,8 +1,11 @@
 export function fizzBuzz(num: number) {
+  if (num > 100) {
+    throw new Error("Number argument should not exceed 100");
+  }
+
   const isMultipleOfThree = num % 3 === 0; 
   const isMultipleOfFive = num % 5 === 0; 
   const isMultipleOfThreeAndFive = isMultipleOfThree && isMultipleOfFive;
-
 
   if (isMultipleOfThreeAndFive) return 'FizzBuzz';
   if (isMultipleOfThree) return 'Fizz';
