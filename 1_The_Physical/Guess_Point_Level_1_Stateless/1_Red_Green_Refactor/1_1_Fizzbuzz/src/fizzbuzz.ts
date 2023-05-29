a2f9213d8dd35b0529132bf27fa92c6147847304
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-05-29 13:43:11 -0500

    [refactor]: extract multiple of 3 and 5 check to boolean flag isMultipleOfThreeAndFive

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,8 +1,9 @@
 export function fizzBuzz(num: number) {
   const isMultipleOfThree = num % 3 === 0; 
+  const isMultipleOfThreeAndFive = num % 3 === 0 && num % 5 == 0;
 
   if (num === 43) return "43";
-  if (num === 15 || num === 45 || num === 90) return 'FizzBuzz';
+  if (isMultipleOfThreeAndFive) return 'FizzBuzz';
   if (isMultipleOfThree) return 'Fizz';
   return 'Buzz'; 
 }
