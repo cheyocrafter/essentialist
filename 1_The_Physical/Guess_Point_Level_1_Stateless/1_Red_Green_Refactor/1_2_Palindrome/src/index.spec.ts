203aec1f10278332f7530556f08c2b374306eb69
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-05-29 16:25:20 -0500

    [red]: it should know that 'mom' is a plindrome

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,33 @@
+/*
+Grading Checklist
+
+- [ ] I have committed on every single transition from red to green to refactor
+- [ ] I have tests that validate the following statements 
+  - [ ]"mom" returns true
+  - [ ]"Mom" returns true
+  - [ ]"MoM" returns true
+  - [ ]"Momx" returns false
+  - [ ]"xMomx" returns true
+  - [ ]"Was It A Rat I Saw" returns true
+  - [ ]"Never Odd or Even" returns true
+  - [ ]"Never Odd or Even1" returns false 
+  - [ ]"1Never Odd or Even1" returns true
+  - [ ] Once I have made the aforementioned tests pass, I have refactored my test specifications to use it.each() to perform parameterization (see Tip #3 here)
+- [ ] There is no duplication in my test code or my production code
+*/
+
+import { PalindromeChecker } from './index';
 
 describe('palindrome checker', () => {
+  it("should know that 'mom' is a palindrome", () => {
+    // arrange 
+    const input = "mom"; 
+    const palindromeChecker = new PalindromeChecker();
 
+    // action 
+    const result = palindromeChecker.isPalindrome(input);
+
+    // assert 
+    expect(result).toBeTruthy();
+  })
 })
