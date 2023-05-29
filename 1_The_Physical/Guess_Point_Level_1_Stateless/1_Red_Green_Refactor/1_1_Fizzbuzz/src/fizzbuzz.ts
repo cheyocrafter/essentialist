44b19f58829a938950054d0bbd2ea04dc1eda79b
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-05-29 14:01:47 -0500

    [refactor - red]: if num is not multiple of 3 or 5 return the number as a string

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -2,11 +2,10 @@ export function fizzBuzz(num: number) {
   const isMultipleOfThree = num % 3 === 0; 
   const isMultipleOfThreeAndFive = num % 3 === 0 && num % 5 == 0;
 
-  if (num === 43) return "43";
-  if (num === 11) return "11";
-  if (num === 23) return "23";
+
   if (isMultipleOfThreeAndFive) return 'FizzBuzz';
   if (isMultipleOfThree) return 'Fizz';
-  return 'Buzz'; 
+  if (!isMultipleOfThree || !(num % 5 === 0)) return num.toString()
+  return 'Buzz';
 }
 
