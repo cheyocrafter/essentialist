563330283f4a24b7797e727426bb807f5b7161a0
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-05-29 13:45:48 -0500

    [green]: returns '11' for 11

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -3,6 +3,7 @@ export function fizzBuzz(num: number) {
   const isMultipleOfThreeAndFive = num % 3 === 0 && num % 5 == 0;
 
   if (num === 43) return "43";
+  if (num === 11) return "11";
   if (isMultipleOfThreeAndFive) return 'FizzBuzz';
   if (isMultipleOfThree) return 'Fizz';
   return 'Buzz'; 
