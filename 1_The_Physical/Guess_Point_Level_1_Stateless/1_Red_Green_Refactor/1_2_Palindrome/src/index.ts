30876e7d9fd82140652b3e8747d689f5397a9c48
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-05-29 19:20:17 -0500

    [refactor]: remove non-palindrome if branches

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,7 +1,5 @@
 export class PalindromeChecker {
   isPalindrome(str: string) {
-    if (str === 'Dwight') return false
-    if (str === 'Momx') return false
     return str.toLowerCase().split('').reverse().join('') === str.toLowerCase();
   }
 }
