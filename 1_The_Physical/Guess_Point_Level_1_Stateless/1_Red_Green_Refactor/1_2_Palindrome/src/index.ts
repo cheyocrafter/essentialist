17a0e4e04d5249859d8c3fed30d7640380df94cd
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-05-29 19:45:27 -0500

    [refactor]: refactor Palindrome checker

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,6 +1,32 @@
 export class PalindromeChecker {
+  private removeSpaces(str: string) {
+    return str.replace(/\s/g, '') 
+  }
+
+  private reverse(str: string) {
+    return str.split('').reverse().join('');
+  }
+
+  private lowerCase(str: string) {
+    return str.toLowerCase();
+  }
+
+  private pipe(...fns: any[]) {
+    return (x: any) => fns.reduce((v, f) => f(v), x);
+  }
+
   isPalindrome(str: string) {
-    str = str.replace(/\s/g, '');
-    return str.toLowerCase().split('').reverse().join('') === str.toLowerCase();
+    let reversed = this.pipe(
+      this.removeSpaces, 
+      this.reverse, 
+      this.lowerCase
+    )(str);
+
+    let notReversed = this.pipe(
+      this.removeSpaces,
+      this.lowerCase
+    )(str)
+
+    return reversed === notReversed;
   }
 }
