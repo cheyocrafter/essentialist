0df62813b552df69ee70d372fbb0c5aea48cb3d9
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-05-29 19:57:34 -0500

    [refactor]: refactored test cases to use it.each

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,8 +1,8 @@
 /*
 Grading Checklist
 
-- [ ] I have committed on every single transition from red to green to refactor
-- [ ] I have tests that validate the following statements 
+- [X] I have committed on every single transition from red to green to refactor
+- [X] I have tests that validate the following statements 
   - [X]"mom" returns true
   - [X]"Mom" returns true
   - [X]"MoM" returns true
@@ -10,107 +10,38 @@ Grading Checklist
   - [X]"xMomx" returns true
   - [X]"Was It A Rat I Saw" returns true
   - [X]"Never Odd or Even" returns true
-  - [ ]"Never Odd or Even1" returns false 
+  - [X]"Never Odd or Even1" returns false 
   - [X]"1Never Odd or Even1" returns true
-  - [ ] Once I have made the aforementioned tests pass, I have refactored my test specifications to use it.each() to perform parameterization (see Tip #3 here)
-- [ ] There is no duplication in my test code or my production code
+  - [X] Once I have made the aforementioned tests pass, I have refactored my test specifications to use it.each() to perform parameterization (see Tip #3 here)
+- [X] There is no duplication in my test code or my production code
 */
 
 import { PalindromeChecker } from './index';
 
 describe('palindrome checker', () => {
-  it("should know that 'mom' is a palindrome", () => {
-    // arrange 
-    const input = "mom"; 
-    const palindromeChecker = new PalindromeChecker();
+  let palindromeChecker: PalindromeChecker; 
 
-    // action 
-    const result = palindromeChecker.isPalindrome(input);
-
-    // assert 
-    expect(result).toBeTruthy();
-  }); 
-
-  it("should know that 'Dwight' is not a palindrome", () => {
-    const input = "Dwight"; 
-    const palindromeChecker = new PalindromeChecker(); 
-
-    const result = palindromeChecker.isPalindrome(input);
-
-    expect(result).toBeFalsy();
-  })
-
-  it("should know that 'Mom' is a palindrome", () => {
-    const input = "Mom"; 
-    const palindromeChecker = new PalindromeChecker(); 
-
-    const result = palindromeChecker.isPalindrome(input);
-
-    expect(result).toBeTruthy();
-  })
-
-  it("should know that 'MoM' is a palindrome", () => {
-    const input = "MoM"; 
-    const palindromeChecker = new PalindromeChecker(); 
-
-    const result = palindromeChecker.isPalindrome(input);
-
-    expect(result).toBeTruthy();
-  })
-
-  it("should know that 'Momx' is not a palindrome", () => {
-    const input = "Momx"; 
-    const palindromeChecker = new PalindromeChecker(); 
-
-    const result = palindromeChecker.isPalindrome(input);
-
-    expect(result).toBeFalsy();
-  })
-
-  it("should know that 'xMomx' is a palindrome", () => {
-    const input = "xMomx"; 
-    const palindromeChecker = new PalindromeChecker(); 
-
-    const result = palindromeChecker.isPalindrome(input);
-
-    expect(result).toBeTruthy();
-  })
-
-  it("should know that 'Was It A Rat I Saw' is a palindrome", () => {
-    const input = "Was It A Rat I Saw"; 
-    const palindromeChecker = new PalindromeChecker(); 
-
-    const result = palindromeChecker.isPalindrome(input);
-
-    expect(result).toBeTruthy();
+  beforeEach(() => {
+    palindromeChecker = new PalindromeChecker();
   });
 
-  it("should know that 'Never Odd or Even' is a palindrome", () => {
-    const input = "Never Odd or Even"; 
-    const palindromeChecker = new PalindromeChecker(); 
-
-    const result = palindromeChecker.isPalindrome(input);
-
-    expect(result).toBeTruthy();
+  it.each([
+    ["mom"],
+    ["Mom"],
+    ["MoM"],
+    ["xMomx"],
+    ["Was It A Rat I Saw"],
+    ["Never Odd or Even"],
+    ["1Never Odd or Even1"],
+  ])("should know that '%s' is a palindrome", (input) => {
+    expect(palindromeChecker.isPalindrome(input)).toBeTruthy();
   });
 
-  it("should know that '1Never Odd or Even1' is a palindrome", () => {
-    const input = "1Never Odd or Even1"; 
-    const palindromeChecker = new PalindromeChecker(); 
-
-    const result = palindromeChecker.isPalindrome(input);
-
-    expect(result).toBeTruthy();
+  it.each([
+    ["Dwight"],
+    ["Momx"],
+    ["Never Odd or Even1"],
+  ])("should know that '%s' is not a palindrome", (input) => {
+    expect(palindromeChecker.isPalindrome(input)).toBeFalsy();
   });
-
-  it("should know that 'Never Odd or Even1' is a palindrome", () => {
-    const input = "Never Odd or Even1"; 
-    const palindromeChecker = new PalindromeChecker(); 
-
-    const result = palindromeChecker.isPalindrome(input);
-
-    expect(result).toBeFalsy();
-  });
-
-
-})
+});
