0b961c8490e248a75acc618e6cd081534c301246
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-05-29 19:26:07 -0500

    [green]: it should know that 'Was It A Rat I Saw' is a palindrome

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,5 +1,6 @@
 export class PalindromeChecker {
   isPalindrome(str: string) {
+    str = str.replace(/\s/g, '');
     return str.toLowerCase().split('').reverse().join('') === str.toLowerCase();
   }
 }
