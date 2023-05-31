6f5801295fd398342fa44ccedf7039fdc4feaec1
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-05-31 15:26:06 -0500

    [new]: it knows that asX6 is NOT between 5 and 15 characters long

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -9,4 +9,11 @@ describe('password validator', () => {
     expect(result.errors).toHaveLength(0);
     expect(result.isValid).toBeTruthy();
   });
+
+  it('knows that "asX6" is NOT between 5 and 15 characters long', () => {
+    const result = PasswordValidator.validate("asX6");
+    expect(result.errors).toHaveLength(1);
+    expect(result.errors[0].type).toMatch(/INVALID_LENGTH_ERROR/);
+    expect(result.isValid).toBeFalsy(); 
+  })
 })
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -1,8 +1,14 @@
 export class PasswordValidator {
   static validate(password: string) {
+    let errors = [];
+
+    if (!(password.length >= 5 && password.length <= 15)) {
+      errors.push({ type: 'INVALID_LENGTH_ERROR' });
+    }
+
     return {
-      isValid: true, 
-      errors: []
+      isValid: errors.length === 0,
+      errors: errors, 
     }
   }
 }
