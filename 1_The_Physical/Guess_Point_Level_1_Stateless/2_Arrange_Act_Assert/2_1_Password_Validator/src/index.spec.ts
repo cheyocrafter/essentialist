b79ca7c7d7a08fe2e317d562c5c90d977291e8b4
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-05-31 16:45:16 -0500

    [new]: should return false for 'as39530376'

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -17,21 +17,25 @@ describe('password validator', () => {
     });
   });
 
-  it('should return true because "Passw4" contains at least one digit', () => {
-    const result = PasswordValidator.validate("Passw4")
-    expect(result.errors).toHaveLength(0);
-    expect(result.isValid).toBeTruthy();
-  }); 
+  describe('checks password contains at least one digit', () => {
+    it.each([
+      { password: "Passw4", isValid: true, errors: [] },
+      { password: "Password", isValid: false, errors: [{ type: "NO_DIGIT_ERROR" }] },
+      { password: "maxwellTheBe", isValid: false, errors: [{ type: 'NO_DIGIT_ERROR'}] },
+    ])
+    ("should return $isValid for '$password'",
+    ({ password, isValid, errors }) => {
+      const result = PasswordValidator.validate(password);
+      expect(result.isValid).toBe(isValid);
+      expect(result.errors).toStrictEqual(errors);
+    });
+  });
 
-  it('should return false because "Password" does not contain at least one digit', () => {
-    const result = PasswordValidator.validate("Password")
-    expect(result.isValid).toBeFalsy();
-    expect(result.errors).toHaveLength(1);
-    expect(result.errors).toStrictEqual([{ type: "NO_DIGIT_ERROR" }])
-  }); 
-
-  it('should return false because "maxwellTheBe" does not contain at least one digit', () => {
-    
-  })
-
+  describe('checks password contains at least one uppercase letter', () => {
+    it('should return false for "as39530376"', () => {
+      const result = PasswordValidator.validate("as39530376");
+      expect(result.isValid).toBeFalsy();
+      expect(result.errors).toStrictEqual([{ type: 'NO_UPPERCASE_LETTER_ERROR' }]);
+    });
+  });
 });
