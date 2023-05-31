755b4ee1bbe0543d5d3c00d89c1a0814bd5dd8f4
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-05-31 16:03:48 -0500

    [chore]: removed comments

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -15,31 +15,8 @@ describe('password validator', () => {
       expect(result.isValid).toBe(isValid);
       expect(result.errors).toStrictEqual(errors);
     });
-  })
+  });
 
 
 
-
-  // it('knows that "Passw4rd8H2vB72" is between 5 and 15 characters long', () => {
-  //   // arrange and act
-  //   const result = PasswordValidator.validate("Passw4rd8H2vB72")
-
-  //   // assert
-  //   expect(result.errors).toHaveLength(0);
-  //   expect(result.isValid).toBeTruthy();
-  // });
-
-  // it('knows that "asX6" is NOT between 5 and 15 characters long', () => {
-  //   const result = PasswordValidator.validate("asX6");
-  //   expect(result.errors).toHaveLength(1);
-  //   expect(result.errors[0].type).toMatch(/INVALID_LENGTH_ERROR/);
-  //   expect(result.isValid).toBeFalsy(); 
-  // });
-
-  // it('knows that "asX387484945939383779530376" is NOT between 5 and 15 characters', () => {
-  //   const result = PasswordValidator.validate("asX387484945939383779530376");
-  //   expect(result.errors).toHaveLength(1);
-  //   expect(result.errors[0].type).toMatch(/INVALID_LENGTH_ERROR/);
-  //   expect(result.isValid).toBeFalsy();
-  // });
 });
