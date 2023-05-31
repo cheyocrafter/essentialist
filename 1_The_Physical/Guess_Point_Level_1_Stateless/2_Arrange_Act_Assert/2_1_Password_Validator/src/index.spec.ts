efb5fe6479337aadcf5f5851b192aa8364b6560b
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-05-31 15:11:53 -0500

    [red]: it knows that "Passw4rd8H2vB72" is between 5 and 15 characters long

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -1,5 +1,12 @@
+import { PasswordValidator } from './index';
 
 describe('password validator', () => {
+  it('knows that "Passw4rd8H2vB72" is between 5 and 15 characters long', () => {
+    // arrange and act
+    const result = PasswordValidator.validate("Passw4rd8H2vB72")
 
+    // assert
+    expect(result.errors).toHaveLength(0);
+    expect(result.isValid).toBeTruthy();
+  });
 })
-
