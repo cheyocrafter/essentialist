9477b0dfaa83066bf8e7caf57402f5d72e453593
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-05-31 16:03:02 -0500

    [refactor]: refactored password length validation using parameterization with it.each

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -1,26 +1,45 @@
 import { PasswordValidator } from './index';
 
 describe('password validator', () => {
-  it('knows that "Passw4rd8H2vB72" is between 5 and 15 characters long', () => {
-    // arrange and act
-    const result = PasswordValidator.validate("Passw4rd8H2vB72")
 
-    // assert
-    expect(result.errors).toHaveLength(0);
-    expect(result.isValid).toBeTruthy();
-  });
+  describe('password length is between 5 and 15 characters long', () => {
+    it.each([
+      { password: "Passw4rd8H2vB72", isValid: true, errors: [] },
+      { password: "Passw4", isValid: true, errors: [] },
+      { password: "asX6", isValid: false, errors: [{ type: 'INVALID_LENGTH_ERROR'}] },
+      { password: "asX387484945939383779530376", isValid: false, errors: [{ type: 'INVALID_LENGTH_ERROR'}] },
+    ])
+    ("should return $isValid for '$password'",
+    ({ password, isValid, errors }) => {
+      const result = PasswordValidator.validate(password);
+      expect(result.isValid).toBe(isValid);
+      expect(result.errors).toStrictEqual(errors);
+    });
+  })
 
-  it('knows that "asX6" is NOT between 5 and 15 characters long', () => {
-    const result = PasswordValidator.validate("asX6");
-    expect(result.errors).toHaveLength(1);
-    expect(result.errors[0].type).toMatch(/INVALID_LENGTH_ERROR/);
-    expect(result.isValid).toBeFalsy(); 
-  });
 
-  it('knows that "asX387484945939383779530376" is NOT between 5 and 15 characters', () => {
-    const result = PasswordValidator.validate("asX387484945939383779530376");
-    expect(result.errors).toHaveLength(1);
-    expect(result.errors[0].type).toMatch(/INVALID_LENGTH_ERROR/);
-    expect(result.isValid).toBeFalsy();
-  });
+
+
+  // it('knows that "Passw4rd8H2vB72" is between 5 and 15 characters long', () => {
+  //   // arrange and act
+  //   const result = PasswordValidator.validate("Passw4rd8H2vB72")
+
+  //   // assert
+  //   expect(result.errors).toHaveLength(0);
+  //   expect(result.isValid).toBeTruthy();
+  // });
+
+  // it('knows that "asX6" is NOT between 5 and 15 characters long', () => {
+  //   const result = PasswordValidator.validate("asX6");
+  //   expect(result.errors).toHaveLength(1);
+  //   expect(result.errors[0].type).toMatch(/INVALID_LENGTH_ERROR/);
+  //   expect(result.isValid).toBeFalsy(); 
+  // });
+
+  // it('knows that "asX387484945939383779530376" is NOT between 5 and 15 characters', () => {
+  //   const result = PasswordValidator.validate("asX387484945939383779530376");
+  //   expect(result.errors).toHaveLength(1);
+  //   expect(result.errors[0].type).toMatch(/INVALID_LENGTH_ERROR/);
+  //   expect(result.isValid).toBeFalsy();
+  // });
 });
