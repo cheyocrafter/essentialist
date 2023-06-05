41e83e6da257c409f64b44bc978381fd69e1f7d4
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-06-05 16:08:12 -0500

    feat: it knows that the average of [1, 2, 3] is 2

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
@@ -4,7 +4,10 @@ export class StatsCalculator {
     return {
       min: sequence.sort((a, b) => a - b)[0],
       max: sequence.sort((a, b) => a - b)[sequence.length-1],
-      count: sequence.length
+      count: sequence.length,
+      average: sequence.reduce((acc, curr) => {
+        return acc + curr
+      }, 0) / sequence.length,
     }
   }
 }
