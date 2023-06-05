c160dd9b2f2f2ae900e180325189095e665a3ebb
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-06-05 15:38:47 -0500

    it should know that the minimum value is -8

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -5,4 +5,10 @@ describe('stats calculator', () => {
     const result = StatsCalculator.calculate([1, 2, 3]);
     expect(result.min).toBe(1);
   });
+
+  it('should know that -8 is the minimum value', () => {
+    const result = StatsCalculator.calculate([2, 4, 21, -8, 53, 40]);
+    expect(result.min).toBe(-8);
+  });
+
 });
