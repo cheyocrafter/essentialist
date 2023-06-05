777411a83f1d3d298bf21ed2be1ec148d9728fe8
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-06-05 15:49:58 -0500

    feat: it knows that 3 is the maximum value

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
@@ -2,7 +2,8 @@ export class StatsCalculator {
   static calculate(sequence: number[]) {
 
     return {
-      min: sequence.sort((a, b) => a - b)[0]
+      min: sequence.sort((a, b) => a - b)[0],
+      max: sequence.sort((a, b) => a - b)[sequence.length-1],
     }
   }
 }
