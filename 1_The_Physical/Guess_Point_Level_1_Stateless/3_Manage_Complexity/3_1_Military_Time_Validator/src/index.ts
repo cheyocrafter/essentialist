22c4991269840219821d48714470d84f751bd42e
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-06-07 14:15:21 -0500

    feat: it knows that "23:00 - 23:56" is valid

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,3 +1,14 @@
+export const hasValidTimePattern = (time: string) => {
+  const [hours, minutes] = time.split(":").map(Number);
+  if (!(hours >= 0 && hours <= 23)) return false; 
+  if (!(minutes >= 0 && minutes <= 59)) return false;
+  return true;
+}
+
 export const validateTimeRange = (timeRange: string) => {
-  return false;
+  const [startTime, endTime] = timeRange.split(' - ');
+  return (
+    hasValidTimePattern(startTime) && 
+    hasValidTimePattern(endTime)
+  );
 }
