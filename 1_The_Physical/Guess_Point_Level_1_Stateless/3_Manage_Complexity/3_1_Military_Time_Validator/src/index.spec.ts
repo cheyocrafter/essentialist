392c28d96b01d842e10ea5cd13ef39c021166039
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-06-07 14:01:36 -0500

    feat: it knows that "25:00 - 12:23" is invalid

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,5 +1,9 @@
+import { validateTimeRange } from './index';
 
 describe('military time validator', () => {
-
-
-})
+  it('it knows that "25:00 - 12:23" is invalid b/c of the start time', () => {
+    const input = "25:00 - 12:23";
+    const result = validateTimeRange(input);
+    expect(result).toBeFalsy();
+  });
+});
