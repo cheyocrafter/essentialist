e7bb4a4226937c1f5689b557ac482a3f25234d79
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-06-10 13:47:53 -0500

    feat: it evalutes "NOT FALSE" as true

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -1,6 +1,7 @@
 
 
 export const booleanCalculator = (expression: string) => {
+  if (expression === "NOT FALSE") return true; 
   if (expression === "NOT TRUE") return false; 
   if (expression === "TRUE") return true;
   if (expression === "FALSE") return false;
