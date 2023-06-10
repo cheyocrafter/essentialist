0be484b046684f60ff9029661ef7184674ce325d
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-06-10 13:38:31 -0500

    feat: it evaluates "TRUE" as true

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,4 +1,9 @@
+import { booleanCalculator } from './index';
 
 describe('boolean calculator', () => {
-
-})
+  it('evaluates "TRUE" as true', () => {
+    const expression = "TRUE";
+    const result = booleanCalculator(expression);
+    expect(result).toBe(true);
+  });
+});
---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -0,0 +1,5 @@
+
+
+export const booleanCalculator = (expression: string) => {
+  return true;
+};
