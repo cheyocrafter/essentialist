33e38b28ff183a669c8e117511ff031646702dc3
Cheyo Crafter <cheyocrafter@users.noreply.github.com>
2023-07-11 14:16:17 -0500

    feat: add user controller

--- 0_From_Code_First_To_First_Principles/1_code_first/assignment/src/index.ts
+++ 0_From_Code_First_To_First_Principles/1_code_first/assignment/src/index.ts
@@ -7,6 +7,9 @@ import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
 
+import userController from './controllers/user.controller';
+import { errorMiddleware } from './middleware/error.middleware';
+
 const PORT = process.env.PORT || 8080;
 
 const app = express();
@@ -18,6 +21,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(compression());
 
+app.use('/users', userController)
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
     console.log(`Application listening on port ${PORT}`);
